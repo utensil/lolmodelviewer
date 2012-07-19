7b1ba98b398103b281bb75b301c862c70a047e30
James <j.lammlein@gmail.com>
2012-07-19 15:19:17 -0400

    Check to make sure the number of bones in ANM files matches the number of bones in SKL file.

--- LOLViewer/GL/GLRenderer.cs
+++ LOLViewer/GL/GLRenderer.cs
@@ -1025,7 +1025,17 @@ namespace LOLViewer
                     bool anmResult = ANMReader.Read(a.Value, ref anmFile);
                     if (anmResult == true)
                     {
-                        animationFiles.Add(a.Key, anmFile);
+                        //
+                        // Not exactly sure what to do when the number of bones
+                        // do not match between ANM and SKL files.
+                        //
+                        // For now, just ignoring the animation file.
+                        //
+
+                        if (anmFile.numberOfBones == sklFile.numBoneIDs)
+                        {
+                            animationFiles.Add(a.Key, anmFile);
+                        }
                     }
                 }
 
